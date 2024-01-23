984cf6566acc0db22c64db35c294260c81a8a634
Muhammad Maaz <129584175+MaazBinNaimat@users.noreply.github.com>
2024-01-23 21:29:06 +0500

    Add project files.

---
+++ Operators task/Operators task/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace Operators_task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            int num2;
+            char operators;
+            int result;
+
+            Console.WriteLine("Enter First Value");
+            num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Second Value");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter an operator (+, -, *, /):");
+            operators = Console.ReadLine()[0];
+
+
+            // Console.WriteLine(num1);
+            // Console.WriteLine(num2);
+            // Console.WriteLine($"You Operator: {operators}");
+
+            if (operators == '+')
+            {
+                result = num1 + num2;
+            }
+            else if (operators == '-')
+            {
+                result = num1 - num2;
+            }
+            else if (operators == '*')
+            {
+                result = num1 * num2;
+            }
+            else if (operators == '/')
+            {
+                result = num1 / num2;
+                
+            }
+            else
+            {
+                Console.WriteLine("Invalid operator!");
+                return;
+            }
+
+            Console.WriteLine($"Result: {result}");
+
+        }
+    }
+}
---
+++ swap values/swap values/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace swap_values
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Initial values
+            int a = 10;
+            int b = 20;
+
+            Console.WriteLine($"Before swapping: a = {a}, b = {b}");
+
+            // Swapping values
+            int c = a;
+            a = b;
+            b = c;
+
+            Console.WriteLine($"After swapping: a = {a}, b = {b}");
+        }
+    }
+}
